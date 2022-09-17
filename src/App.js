38b270910965c47712e346f6d414d33de449b199
4geru <westhouse51@gmail.com>
2022-09-17 21:32:31 +0900

    add login and logout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+
 import './App.css';
+import liff from '@line/liff'
+import { useState, useEffect } from 'react';
+const user = {
+  displayName: 'Hedy Lamarr',
+  pictureUrl: 'https://i.imgur.com/yXOvdOSs.jpg'
+};
+
+const Profile = ({login}) => {
+  const [profile, setProfile] = useState(user);
+  useEffect(() => {
+    if(liff.isLoggedIn()) {
+      liff.getProfile().then((fetchedProfile) => {
+        setProfile({
+          displayName: fetchedProfile.displayName,
+          pictureUrl: fetchedProfile.pictureUrl
+        })
+      })  
+    } else {
+      setProfile(user)
+    }  
+  }, [login])
+  return (
+    <>
+      <h1>{profile.displayName}</h1>
+      <img
+        className="avatar"
+        src={profile.pictureUrl}
+        alt={'Photo of ' + profile.displayName}
+        style={{
+          width: 90,
+          height: 90
+        }}
+      />
+    </>
+  );
+}
+
+const LoginButton = ({setLogin}) => {
+  const click = () => {
+    if (!liff.isLoggedIn()) {
+      liff.login().then(() => {
+        setLogin(true)
+      })
+    }
+  }
+  return (
+    <button onClick={click}>login</button>
+  );
+}
+
+const LogoutButton = ({setLogin}) => {
+  const click = () => {
+    liff.logout()
+    setLogin(false)
+  }
+  return (
+    <button onClick={click}>logout</button>
+  );
+}
 
 function App() {
+  const [login, setLogin] = useState(undefined);
+  useEffect(() => {
+    setLogin(liff.isLoggedIn())
+  }, [login])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my app</h1>
+      { !login && '未ログイン' }
+      { login && 'ログイン' }
+      { login ?
+        <LogoutButton setLogin={setLogin} /> :
+        <LoginButton setLogin={setLogin} />
+      }
+      { login && <Profile /> }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import liff from '@line/liff'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+document.addEventListener("DOMContentLoaded", function() {
+  liff
+    .init({ liffId: '1657480741-kYJW0Nev' })
+    .then(() => {
+        console.log("Success! you can do something with LIFF API here.")
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
