e486c489378c6c44d5872777d29997aa1059132e
4geru <westhouse51@gmail.com>
2022-09-19 08:36:36 +0900

    add micro cms functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import env from "react-dotenv";
+const { createClient: createMicroCmsClient } = require('microcms-js-sdk');
+
+const Form = ({content, microCmsClient}) => {
+  const [formContent, setFormContent] = useState(content)
+
+  return <div className={'container'}>
+    <div className='item'>
+      <input
+        type="text"
+        value={formContent.title}
+        onChange={(e) => {
+          setFormContent({...formContent, title: e.target.value })
+        }}
+        placeholder="title"
+      />
+    </div>
+    <div className='item'>
+      <textarea
+        value={formContent.content}
+        onChange={(e) => {
+          setFormContent({...formContent, content: e.target.value })
+        }}
+        rows={5}
+        placeholder="content"
+      />
+    </div>
+    <div className='item'>
+      <button onClick={() => {
+        microCmsClient
+          .update({
+            endpoint: 'liff',
+            contentId: formContent.id,
+            content: {
+              title: formContent.title,
+              content: formContent.content,
+            },
+          })
+          .then((res) => console.log(res.id))
+          .catch((err) => console.error(err));
+      }}>
+        submit
+      </button>
+    </div>
+  </div>
+}
 
 function App() {
+  const [content, setContent] = useState(undefined)
+  const microCmsClient = createMicroCmsClient({ serviceDomain: env.MICRO_CMS_SERVICE_DOMAIN, apiKey: env.MICRO_CMS_API_KEY });
+  useEffect(() => {
+    microCmsClient.get({
+      endpoint: 'liff',
+      queries: {
+        // filter https://document.microcms.io/content-api/get-list-contents#hdebbdc8e86
+        filters: 'userId[equals]4gerugeru'
+      },
+    }).then((res) => {
+      setContent(res.contents[0])
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { content && <Form content={content} microCmsClient={microCmsClient} /> }
     </div>
   );
 }
