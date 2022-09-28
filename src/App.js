3bbaa533847c4af726c4fcd0bc9b5ea7d24d40f9
4geru <westhouse51@gmail.com>
2022-09-29 07:08:46 +0900

    implement microCms create method

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Profile } from './Profile';
 import { LogoutButton } from './LogoutButton';
 import { LoginButton } from './LoginButton';
 import { EditForm } from './EditForm';
+import env from "react-dotenv";
+const { createClient: createMicroCmsClient } = require('microcms-js-sdk');
+
 
 function App() {
   const [liffState, setLiffState] = useState([null, false]);
@@ -13,6 +16,17 @@ function App() {
       .init({ liffId: '1657480741-kYJW0Nev' })
       .then(() => {
         const login = liff.isLoggedIn()
+        const microCmsClient = createMicroCmsClient({ serviceDomain: env.MICRO_CMS_SERVICE_DOMAIN, apiKey: env.MICRO_CMS_API_KEY });
+        microCmsClient.create({
+          endpoint: 'liff',
+          content: {
+            userId: 'gege',
+            title: "wood",
+            content: "複数行のテキストを入力\n複数行のテキストを入力"
+          }
+        }).then((res) => console.log(res.id)
+        ).catch((e) => console.log(e))
+
         setLiffState([liff, login])
       })
       .catch((err) => {
